8ec47a0bf40d9c242d601baa2152a79bb27c8ee5
Roy Mdr <go_rojo@hotmail.com>
2022-06-20 01:08:44 -0500

    Added /connections
    going to /connections allows you to see the number of connections subscribed to a topic. When a connection is made it will notify it.

--- server/index.js
+++ server/index.js
@@ -38,7 +38,8 @@ const separator = "''";
 const state = {
 	nextId: 0,
 	connections: {},
-	subscriptions: {}
+	clids: {},
+	topics: {}
 };
 
 
@@ -178,90 +179,26 @@ function onRequest(request, response) {
 						return;
 					}
 
-					/*if (
-						   jBody.e !== undefined
-						&& jBody.e.info !== undefined
-						&& typeof jBody.e.info !== 'string'
-						) {
-						response.writeHead(400, 'Data should be  string', { 'Access-Control-Allow-Origin' : '*' });
+					try {
+
+						emitEvent(
+							{
+								ep: jBody.ep,
+								e: {
+									type: jBody.e.type,
+									info: jBody.e.info === undefined ? undefined : jBody.e.info
+								}
+							}
+						);
+
+					} catch(err) {
+						response.writeHead(400, err, { 'Access-Control-Allow-Origin' : '*' });
 						response.end();
-						return;
-					}*/
-
-					jBody.ep.forEach( (ePoint, indx, nde) => {
-
-						if (typeof ePoint !== 'string') {
-							response.writeHead(400, 'End Point should be a string', { 'Access-Control-Allow-Origin' : '*' });
-							response.end();
-							return;
-						}
-
-						const arr1 = ePoint.split('/');
-
-						for (const [subsId, subs] of Object.entries(state.subscriptions)) {
-
-							const arr2 = subsId.split('/');
-
-							let matched = true;
-							const times = arr1.length > arr2.length ? arr1.length : arr2.length;
-							for (let i = 0; i < times; i++) {
-
-								if (arr1[i] == undefined || arr2[i] == undefined) {
-									matched = false;
-									break;
-								}
-
-								if (
-									   (arr1[i] == "#" && i == arr1.length - 1)
-									|| (arr2[i] == "#" && i == arr2.length - 1)
-									) {
-									break;
-								}
-
-								if (
-									   (arr1[i] == "+" && i != arr1.length - 1)
-									|| (arr2[i] == "+" && i != arr2.length - 1)
-									) {
-									continue;
-								}
-
-								if (arr1[i] != arr2[i]) {
-									matched = false;
-									break;
-								}
-							}
-
-							if (matched) {
-								for (const [connId, conn] of Object.entries(subs)) {
-
-									const jData = JSON.stringify({
-										iat: Date.now(),
-										payload: {
-											ep: {
-												requested: subsId,
-												emitted: ePoint
-											},
-											e: {
-												type: jBody.e.type,
-												info: jBody.e.info === undefined ? undefined : jBody.e.info
-											}
-										}
-									})
-									.replace(/\'/g, '\\u0027'); // UNICODE ESCAPE: '
-
-									const outData = `${separator}${jData.length}${separator}${jData}`;
-									conn.response.write( outData );
-									console.log( `[${conn._id}]: ${outData}` );
-									resetTick(conn);
-								}
-							}
-						}
-
-					} );
+					}
 				
 					response.writeHead(200, { 'Access-Control-Allow-Origin' : '*' });
 					// console.log( Object.keys(state.connections) );
-					// console.log( state.subscriptions );
+					// console.log( state.topics );
 					response.end();
 
 				});
@@ -270,6 +207,40 @@ function onRequest(request, response) {
 
 			if (request.method != 'OPTIONS' && request.method != 'POST') {
 				response.writeHead(400, 'Method is not POST', { 'Access-Control-Allow-Origin' : '*' });
+				response.end();
+				return;
+			}
+
+			break;
+
+
+
+			case "/connections":
+
+			if (request.method == 'GET') {
+
+				/*************************************************************************************************/
+				/***** EVALUATE AUTHORIZATION TO GET CONNECTIONS (MAYBE ONLY USER CAN SEE OWNED CONNECTIONS) *****/
+				/*************************************************************************************************/
+
+				let clidConnected = 0;
+
+				if ( url_parts.query.clid != undefined && state.clids.hasOwnProperty(url_parts.query.clid) ) {
+					clidConnected = state.clids[url_parts.query.clid];
+				}
+
+				console.log(clidConnected);
+				
+				response.writeHead(200, { 'Access-Control-Allow-Origin' : '*' });
+				// console.log( Object.keys(state.connections) );
+				// console.log( state.topics );
+				response.write(clidConnected.toString());
+				response.end();
+
+			}
+
+			if (request.method != 'GET') {
+				response.writeHead(400, 'Method is not GET', { 'Access-Control-Allow-Origin' : '*' });
 				response.end();
 				return;
 			}
@@ -303,16 +274,48 @@ function trackConnection(parsed_url, parsed_body = {}, request, response) {
 	connection.response = response;
 	connection.query	= parsed_url.query;
 	connection.body		= parsed_body;
+
+
+	if (connection.body.clid == undefined || typeof connection.body.clid != 'string') {
+		connection.body.clid = '@ANON_CONNECTION@';
+	}
+
+	if ( state.clids.hasOwnProperty(connection.body.clid) ) {
+		state.clids[connection.body.clid]++;
+	} else {
+		state.clids[connection.body.clid] = 1;
+	}
+
+	if ( state.clids.hasOwnProperty('@TOTAL@') ) {
+		state.clids['@TOTAL@']++;
+	} else {
+		state.clids['@TOTAL@'] = 1;
+	}
+
+
 	connection.onClose	= (err) => {
-			console.log(`Connection [${connection._id}] closed default.`);
+			console.log(`Connection [${connection._id}](${connection.body.clid}) closed default.`);
 			
 			connection.response.end();
 			delete state.connections[connection._id];
+
+			state.clids[connection.body.clid]--;
+			if (state.clids[connection.body.clid] == 0) {
+				delete state.clids[connection.body.clid];
+			}
+
+			state.clids['@TOTAL@']--;
+			if (state.clids['@TOTAL@'] == 0) {
+				delete state.clids['@TOTAL@'];
+			}
+
 		}
 
 	connection.request.on('close', connection.onClose);
 
+
 	console.log(`Connection ID: ${connection._id}`);
+	console.log(`Client ID: ${connection.body.clid}[${state.clids[connection.body.clid] - 1}]`);
 
 	return connection;
 }
@@ -330,7 +333,7 @@ function handleSubscription(connection) {
 	resetTick(connection);
 
 	connection.timeout	= setTimeout( () => {
-			console.log(`Connection [${connection._id}] hard-coded timed out.`);
+			console.log(`Connection [${connection._id}](${connection.body.clid}) hard-coded timed out.`);
 			connection.request.removeListener('close', connection.onClose);
 			connection.onClose();
 		}, 5 * 60 * 1000);
@@ -338,7 +341,17 @@ function handleSubscription(connection) {
 	connection.request.removeListener('close', connection.onClose);
 	connection.onClose	= (err) => {
 			if (connection !== undefined) {
-				console.log(`Connection [${connection._id}] closed.`);
+				console.log(`Connection [${connection._id}](${connection.body.clid}) closed.`);
+
+				emitEvent({
+					ep: [`${connection.body.clid}/@CONNECTION@`],
+					e: {
+						type: 'disconnect',
+						info: {
+							clid_instances: state.clids[connection.body.clid] - 1
+						}
+					}
+				});
 
 				clearTimeout(connection.timeout);
 				clearTimeout(connection.tick);
@@ -347,14 +360,14 @@ function handleSubscription(connection) {
 				if ( 'ep' in connection.body) {
 					connection.body.ep.forEach( (ePoint, index, node) => {
 
-						if ( ePoint in state.subscriptions ) {
+						if ( ePoint in state.topics ) {
 
-							if ( connection._id in state.subscriptions[ePoint] ) {
-								delete state.subscriptions[ePoint][connection._id];
+							if ( connection._id in state.topics[ePoint] ) {
+								delete state.topics[ePoint][connection._id];
 							}
 
-							if ( Object.keys(state.subscriptions[ePoint]).length == 0) {
-								delete state.subscriptions[ePoint];
+							if ( Object.keys(state.topics[ePoint]).length == 0) {
+								delete state.topics[ePoint];
 							}
 						}
 					});
@@ -363,7 +376,19 @@ function handleSubscription(connection) {
 						delete state.connections[connection._id];
 					}
 				}
+
+				state.clids[connection.body.clid]--;
+				if (state.clids[connection.body.clid] == 0) {
+					delete state.clids[connection.body.clid];
+				}
+
+				state.clids['@TOTAL@']--;
+				if (state.clids['@TOTAL@'] == 0) {
+					delete state.clids['@TOTAL@'];
+				}
+
 				connection = undefined;
+
 			}
 		}
 	connection.request.on('close', connection.onClose);
@@ -374,8 +399,8 @@ function handleSubscription(connection) {
 
 	connection.body.ep.forEach( (ePoint, index, node) => {
 
-		(ePoint in state.subscriptions) || (state.subscriptions[ePoint] = {} )
-		state.subscriptions[ePoint][connection._id] = connection;
+		(ePoint in state.topics) || (state.topics[ePoint] = {} )
+		state.topics[ePoint][connection._id] = connection;
 
 	});
 
@@ -406,7 +431,18 @@ function handleSubscription(connection) {
 		});
 
 	connection.response.write( `${separator}0${separator}` );
-	console.log( `[${connection._id}]: ${separator}0${separator}` );
+	console.log( `[${connection._id}](${connection.body.clid}): ${separator}0${separator}` );
+
+	/* CONNECTED! */
+	emitEvent({
+		ep: [`${connection.body.clid}/@CONNECTION@`],
+		e: {
+			type: 'connect',
+			info: {
+				clid_instances: state.clids[connection.body.clid]
+			}
+		}
+	});
 
 }
 
@@ -424,10 +460,159 @@ function resetTick(connection) {
 
 		connection.tick = setTimeout( () => {
 				connection.response.write( `${separator}0${separator}` );
-				console.log( `[${connection._id}]: ${separator}0${separator}` );
+				console.log( `[${connection._id}](${connection.body.clid}): ${separator}0${separator}` );
 				resetTick(connection);
 			}, 50 * 1000);
 
 	}
+
+}
+
+
+
+
+
+function emitEvent(evObj) {
+
+
+
+	// VALIDATE EVENT OBJECT //
+
+	if (
+		   evObj.ep == undefined
+		|| evObj.e == undefined
+		|| evObj.e.type == undefined
+		|| !Array.isArray(evObj.ep)
+		|| typeof evObj.e.type !== 'string'
+		) {
+
+		throw 'Invalid event object';
+	}
+
+
+
+	// VALIDATE EVENT INFO DATA //
+
+	/*if (
+		   evObj.e !== undefined
+		&& evObj.e.info !== undefined
+		&& typeof evObj.e.info !== 'string'
+		) {
+		response.writeHead(400, 'Data should be  string', { 'Access-Control-Allow-Origin' : '*' });
+		response.end();
+		return;
+	}*/
+
+
+
+	// GET SUBSCRIBERS OF GIVEN TOPICS //
+
+	const subscribers = getSubscribers(evObj.ep);
+
+
+
+	// EMIT EVENT TO EACH SUBSCRIBER //
+
+	subscribers.forEach( (subscriber) => {
+
+		const jData = JSON.stringify({
+			iat: Date.now(),
+			payload: {
+				ep: {
+					requested: subscriber.topicRequested,
+					emitted: subscriber.topicEmitted
+				},
+				e: {
+					type: evObj.e.type,
+					info: evObj.e.info === undefined ? undefined : evObj.e.info
+				}
+			}
+		})
+		.replace(/\'/g, '\\u0027'); // UNICODE ESCAPE: '
+
+		const outData = `${separator}${jData.length}${separator}${jData}`; // FORMAT RESPONSE
+
+		subscriber.connection.response.write( outData ); // SEND DATA TO SUBSCRIBER
+		resetTick(subscriber.connection);
+
+		console.log( `[${subscriber.connection._id}](${subscriber.connection.body.clid}): ${outData}` );
+
+		return true;
+
+	} );
+
+
+
+}
+
+
+
+
+
+function getSubscribers(arrayTopics) { // MQTT-topics Style
+
+	/* THIS FUNCTION RETURNS AN ARRAY OF { TOPICS AND CONNECTIONS } OF THE SUBSCRIBERS
+	   SUBSCRIBED TO THE GIVEN TOPICS IN arrayTopics */
+
+
+
+	const subscribers = [];
+
+	arrayTopics.forEach( (ePoint, indx, nde) => {
+
+		if (typeof ePoint !== 'string') {
+			throw 'End Point should be a string';
+		}
+
+		const arr1 = ePoint.split('/');
+
+		for (const [subsId, subs] of Object.entries(state.topics)) {
+
+			const arr2 = subsId.split('/');
+
+			let matched = true;
+			const times = arr1.length > arr2.length ? arr1.length : arr2.length;
+			for (let i = 0; i < times; i++) {
+
+				if (arr1[i] == undefined || arr2[i] == undefined) {
+					matched = false;
+					break;
+				}
+
+				if (
+					   (arr1[i] == "#" && i == arr1.length - 1)
+					|| (arr2[i] == "#" && i == arr2.length - 1)
+					) {
+					break;
+				}
+
+				if (
+					   (arr1[i] == "+" && i != arr1.length - 1)
+					|| (arr2[i] == "+" && i != arr2.length - 1)
+					) {
+					continue;
+				}
+
+				if (arr1[i] != arr2[i]) {
+					matched = false;
+					break;
+				}
+			}
+
+			if (matched) {
+
+				for (const [connId, conn] of Object.entries(subs)) {
+					subscribers.push({
+						topicRequested: subsId,
+						topicEmitted: ePoint,
+						connection: conn
+					});
+				}
+			}
+		}
+
+	} );
+
+	return subscribers;
 
 }
