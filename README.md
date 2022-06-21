e8ecaba3891877b0f9b929871cc62224d03852dd
Roy Mdr <go_rojo@hotmail.com>
2022-06-20 20:52:54 -0500

    Add NoPollSubscriber_NODEJS.js
    Create NoPollSubscriber_NODEJS.js client library

--- README.md
+++ README.md
@@ -2,5 +2,168 @@
 
 Estudio Sustenta broker for handling real-time notifications
 
+Made before I knew about MQTT and then adopting some of it 😅 so maybe just use MQTT
+
 
 # *RECUERDA AÑADIR LICENCIA ANTES DE HACERLO PÚBLICO*
+
+## To Do
+
+- [ ] Migrate keys to external file
+- [ ] Fix all the mixed up using of "event point", "channel" and "topic"
+- [ ] Organize code
+- [ ] Add pushing quee when multiple event are ammited to the same endpoint. Messages colliding?
+- [ ] Add verify the server sending event
+- [ ] Migrate to stateless JWT auth with access and refresh token workflow
+- [ ] Add redis integration for handling stateful auth
+- [ ] Add websockets API
+- [ ] Add raw TCP API
+
+## API
+
+`/`
+Handles client subscription
+
+`/event`
+Handles event sent by a server
+
+`/connections`
+List of active clients and number of instances for each `client_id` (and `@TOTAL@`).
+If no `client_id` was passed to the subscription it will default to `@ANON_CONNECTION@`
+
+`/connections?clid=<client_id>`
+Number of active instances of given `client_id`
+
+`/connections?clid=<client_id>&details=true`
+Insight of full details of active instances of given `client_id`
+
+## FLUJO
+
+### CLIENTE SE SUSCRIBE
+Petición `POST` a `NOTIF. SERVER` en donde se especifiquen los `EVENT POINTS` que se quieren escuchar (a los que se va a suscribir)
+```js
+// SUB CONFIG
+{
+  bind_key: BindKey, // Optional
+  clid: String, // Optional, helps identify client for debugging
+  ep: [
+    /* Setup as string or object with properties */
+    TopicString,
+    {
+      topic: TopicString,
+      binded: true // Optional. Only works if a valid bind_key is passed
+    }
+  ]
+}
+```
+Al pasar una `BindKey` válida, se habilitan los `Event Points` permitidos para el usuario identificado.
+> Ej. `Usuario_1` y `Usuario_2` se suscriben a `CASA/NIVEL_AGUA` y cada uno pasa su `BinKey`, pero solo `Usuario_1` tiene permitido recibir esa información, por lo que `Usuario_2` nunca recibe la notificación.
+
+`BindKey` = RSA_ENCRYPTED({iat, session_id})
+> Las BindKey tienen una expiración corta (~3seg) configurada en broker
+
+Flujo:
+1. cliente web pide a servidor PHP el `BindKey` pasando su `PHP_SESION_ID` en una cookie (HTTPS ONLY)
+1. cliente web se suscribe usando el `BindKey` recibido
+1. `NOTIF. SERVER` envia `BindKey` a servidor PHP para que este devuelva al `NOTIF. SERVER` el `USER_ID` asociado a la sesión
+1. `NOTIF. SERVER` bindea usuario con conexion y tópicos
+
+(esto se podría evitar si ambos servidores compartiran storage de session_id o se hiciera uso de JWT)
+
+
+### SE EMITE EVENTO
+>SOLO LO PUEDE HACER UN BACKEND! EL MISMO QUE MODIFICA BASES DE DATOS
+
+peticion `POST` a `NOTIF. SERVER` en donde se especifiquen los `EVENT POINTS` a los que se notificara y el `EVENTO` que contendrá el `TIPO DE EVENTO` y opcionalmente los `DATOS DE EVENTO` (String JSON encoded si es necesario)
+```js
+// EVENT
+{
+  bind_key: BindKey, // Optional
+  ep: [
+    /* Setup as string or object with properties */
+    TopicString, // Unbinded, shout mode event
+    {
+      topic: TopicString,
+      whisper: ConnId, // Optional. Connection Id to whisper event
+      binded: true, // Optional. Only works if a valid bind_key is passed
+      password: String // Optional. Only if endpoint requires password
+    }
+  ],
+  e: {
+    type: String,
+    detail: Any // Optional
+  }
+}
+```
+Al pasar una `BindKey` válida, los `EVENT POINTS` serán dirigidos sólo a los usuarios que los pueden escuchar. De igual manera, seran evaluados los permisos para emitir el evento a los `EVENT POINTS` pasados.
+
+`BindKey` = RSA_ENCRYPTED({iat, user_id})
+> Las BindKey tienen una expiración corta (~3seg) configurada en broker
+
+Flujo:
+1. cliente envía evento a servidor PHP destinado. Pasa su `PHP_SESION_ID` en una cookie (HTTPS ONLY)
+1. servidor PHP evalúa si cliente tiene acceso a este endpoint (en base a sesión)
+1. servidor PHP extrae `USER_ID` de `PHP_SESION_ID`, genera el `BindKey` y emite evento a `NOTIF. SERVER`
+1. `NOTIF. SERVER` desencripta `BindKey` y si el `timestamp` no ha expirado (es menor al tiempo actual, pero mayor a el tiempo actual menos X segundos), revisa permisos de `USER_ID` recibido para publicar a cada `EVENT POINT`
+(esto se podría evitar si ambos servidores compartiran storage de session_id o se hiciera uso de JWT)
+(AMBOS SERVIDORES DEBEN ESTAR PERFECTAMENTE SINCRONIZADOS EN SU RELOJ)
+
+
+### [EN BASE DE DATOS]
+Al momento de publicar o suscribirse de forma bindeada, sólo entran en juego los endpoints registrados en esta base de datos.
+```js
+// BINDED EVENT POINT
+{
+  ep: String, // Atomic EventPoint
+  owner: UserId,
+  pub_password: HashString, // Empty = no password required
+  pub: [ // Who can publish to this topic?
+    UserId // Includes owner. WILDCARDS: "[][][]" = all unidentified (binded) users, "+" = all binded users, "*" = all users
+  ],
+  sub: [ // Who can subscribe to this topic?
+    UserId // Includes owner. WILDCARDS: "[][][]" = all unidentified (binded) users, "+" = all binded users, "*" = all users
+  ]
+}
+```
+> !!! EVITA REGISTRAR `EVENT POINTS` CON `WILDCARDS` (# o +) EN LA BASE DE DATOS
+
+
+## EVENT POINTS ESPECIALES / RESERVADOS
+
+### @SERVER@
+Emitido al realizar la conexión exitosamente. Contiene metadatos de la conexión asignada al cliente.
+
+### @CONNECTION@/clid
+Escucha las conexiones y desconexiones del Id de cliente suministrado
+Al suscribirse emite un evento con el número de conexiones del clid actualmente conectadas.
+
+## SHOUT & WHISPER [CONCEPTO]
+Distintos clientes pueden estar suscritos a un mismo `EVENT POINT`, por lo tanto cada evento que se emita a dicho `EVENT POINT` será recibido por los clientes suscritos a este. Esto se conocerá como un evento en modo `SHOUT`.
+Sin embargo el emisor del evento puede decidir emitirlo como modo `WHISPER` en donde el evento será recibido sólo por un cliente (especificado por el emisor). Esto hace que se tenga una fuincionalidad prácticamente de un tradicional HTTP donde hay peticiones y respuestas a quien hizo la petición.
+
+Si no se recibe instrucción de `WHISPER`, se asume modo `SHOUT`.
+
+
+## TIPOS DE NOTIFICACION
+### BY PAGE
+		- Definido en peticion de cliente
+		- Significa que el hecho de estar en esta pagina, te suscribe a sus eventos
+		- Al salir de la pagina te des-suscribes de sus eventos
+		- Se recibe cualquier usuario en la pagina
+
+		- ej. Un espectador de una partida en especifico de un juego online: recibe las actualizaciones de los jugadores y del chat publico. Fuera de la sala no recibe notificaciones de la partida.
+
+### USER TARGETED
+		- Definido en BASE DE DATOS de cada evento
+		- Un evento X revisa en BASE DE DATOS quienes son los usuarios suscritos (a quienes se les notificará el evento) y manda una señal a cada uno de sus EVENT POINTS
+		- Se recibe independiente de la pagina actual (recibira notificacion en cualquier sitio)
+		- Para anular la subscripcion se debe modificar la BASE DE DATOS del evento correspondiente
+
+		- ej. Se actualiza un post al que estas suscrito: recibes la notificacion de que ocurrio sin importar que estes en ese post
+
+### USER TARGETED BY PAGE
+		- Definido en peticion de cliente acompañado de TOKEN / SESSID como autenticacion
+		- Solo se recibe si el usuario esta en la pagina
+		- Para dejar de escuchar los eventos se puede modificar la BASE DE DATOS del evento (des-suscribirse) o abandonar la pagina actual
+
+		- ej. Un jugador de un juego online: recibe las actualizaciones de sus rivales y su chat personal. Fuera de la sala no recibe notificaciones de la partida.
---
+++ client/NoPollSubscriber_NODEJS.js
@@ -0,0 +1,694 @@
+const http  = require('http');
+const https = require('https');
+// require('https').globalAgent.options.ca = require('ssl-root-cas/latest').create();
+
+class NoPollSubscriber {
+	/************************************************
+	          SHOULD LISTEN FOR VALID JSON,          
+	         {"iat":Unixtime,"payload":Object}         
+	    -----------------------------------------
+	  To auto-start subscription ( .start() )
+	  should be inside the window.onload function,
+	  otherwise Firefox will not finish the
+	  page loading indicator.
+	************************************************/
+
+
+
+	/////////////////////////////////////////////////
+	////////////////// CONSTRUCTOR //////////////////
+
+	constructor(urlOrObj, callbackOnParsed, callbackOnSubscribe = ()=>{}, callbackOnStateChange = ()=>{}) {
+
+		// PRIVATE ATTRIBUTES //
+		this._XHR          = this._setHTTPRequest(urlOrObj);
+		this._bindURL      = '';
+		this._onParsed     = callbackOnParsed;
+		this._onSubscribe  = callbackOnSubscribe;
+		this._onStChange   = callbackOnStateChange;
+		this._responseLen  = 0;
+		this._dataMem      = '';
+		this._fullResponse = null;
+		this._subscribed   = false;
+		this._stopped      = false;
+		this._separator    = "''";
+		this._httpCall     = null;
+		this._retryTimeout = setTimeout(() => {}, 0);
+		this._lastIat      = 0;
+		this._resTimeout   = setTimeout(() => {}, 0);
+
+		// SET EVENT LISTENERS //
+		this._addOnlineListeners();
+		// this._remOnlineListeners();
+
+	}
+
+
+
+
+
+	/////////////////////////////////////////////////
+	//////////////// PUBLIC METHODS ////////////////
+
+	start( urlOrObj              = this._XHR,
+	       callbackOnParsed      = this._onParsed,
+	       callbackOnSubscribe   = this._onSubscribe,
+	       callbackOnStateChange = this._onStChange ) { // Start, restart and resume paused
+
+		this.stop();
+		this.abort();
+		clearTimeout(this._retryTimeout);
+		this.setURL(urlOrObj);
+
+		this._stopped      = false;
+		this._onParsed     = callbackOnParsed;
+		this._onSubscribe  = callbackOnSubscribe;
+		this._onStChange   = callbackOnStateChange;
+
+		this._subscribe();
+	}
+
+	stop() { // Wont call again when ready state == 4
+		this._stopped = true;
+	}
+
+	abort() { // Abort connection (will retry, so to fully stop: fist stop() then abort())
+		clearTimeout(this._retryTimeout);
+		if (this._httpCall) this._httpCall.destroy();
+		this._dataMem = '';
+	}
+
+	validateIat(rawString) { // Experimental...
+
+		if ( typeof rawString !== 'string' ) {
+			console.error('Parameter should be a JSON encoded string.');
+			return;
+		}
+
+		this._validateIat(rawString);
+	}
+
+
+
+
+
+	/////////////////////////////////////////////////
+	//////////////// PRIVATE METHODS ////////////////
+
+	_addOnlineListeners() {
+		// window.NoPollSubscriber = this;
+		// window.addEventListener('online',  this._isOnline );
+		// window.addEventListener('offline', this._isOffline);
+	}
+
+	_remOnlineListeners() {
+		// delete window.NoPollSubscriber;
+		// window.removeEventListener('online',  this._isOnline );
+		// window.removeEventListener('offline', this._isOffline);
+	}
+
+	_isOnline(ev) {
+		// console.log('came online');
+		// console.log(this);
+		// console.log(ev);
+		ev.target.NoPollSubscriber.start();
+	}
+
+	_isOffline(ev) {
+		// console.log('came offline');
+		// console.log(this);
+		// console.log(ev);
+		ev.target.NoPollSubscriber.stop();
+		ev.target.NoPollSubscriber.abort();
+		ev.target.NoPollSubscriber._onStChange({value:-1,state:"OFFLINE"});
+	}
+
+	_setHTTPRequest(request) {
+
+		if ( (typeof request !== 'string') && (typeof request !== 'object') ) {
+			console.error('What are you requesting?', 'Request should be a string or an object with options.');
+			return;
+		}
+
+		if (typeof request === 'string') {
+			const url = request;
+			request = {};
+			request.url = url;
+		}
+
+		if (typeof request === 'object') {
+
+			if ( !('url' in request) ) {
+				console.error('should pass url');
+				return;
+			}
+
+			if ( typeof request.url !== 'string') {
+				console.error('url should be a string');
+				return;
+			}
+
+			if ( ('method' in request) && typeof request.method !== 'string') {
+				console.error('method should be a string');
+				return;
+			}
+
+			if ( ('data' in request) && typeof request.data !== 'string') {
+				request.data = JSON.stringify(request.data);
+			}
+
+			if ( 'headers' in request ) {
+				if (typeof request.headers !== 'object') {
+					console.error('headers should be an object');
+					return;
+				} else {
+					for ( const key in request.headers ) {
+						if (typeof request.headers[key] !== 'string') {
+							console.error(`header '${key}' should be a string`);
+							return;
+						}
+					}
+				}
+			}
+
+			if ( 'dataType' in request ) {
+
+				if (typeof request.dataType !== 'string') {
+					console.error('dataType should be a string');
+					return;
+				} else {
+					('headers' in request) || (request.headers = {})
+					request.headers['Content-Type'] = request.dataType;
+				}
+				
+			}
+		}
+
+		const xhr = {
+			url:     request.url,
+			method:  request.method  || 'GET',
+			data:    request.data    || undefined,
+			headers: request.headers || {}
+		}
+		
+		// console.log(xhr);
+
+		return xhr;
+	}
+
+	_logicOnChunk() {
+		const obj = this;
+
+		let payload = '';
+		const response = obj._httpCall.responseText.substring(obj._responseLen);
+		const bUpdated = obj._httpCall.responseText.length - obj._responseLen;
+		obj._responseLen = obj._httpCall.responseText.length;
+
+		
+		/*
+		// Very useful logging
+		console.log(
+			response,
+			`(${bUpdated} bytes updated)`,
+			`(${obj._responseLen} bytes total)`,
+			`(HTTP response state: ${obj._httpCall.readyState})`);
+		*/
+
+		if ( obj._dataMem.length > 0 ) { // If there is data in memory
+			payload = obj._dataMem + response; // Add it to the beginning
+		} else {
+			payload = response;
+		}
+
+		obj._dataMem = ''; // Empty memory
+
+		obj._parseChunks( payload.split(obj._separator) );
+	}
+
+
+	_parseChunks(array) {
+		const obj = this;
+
+		let expLen = 0;
+
+		if ( array[0].replace(/\s/g, '').length == 0) { array.shift(); }
+
+		if ( array.length == 0 ) {
+			return false;
+		}
+
+		if ( array.length == 1 ) {
+			obj._dataMem = array[0];
+			return false;
+		}
+
+		expLen = array.shift();
+
+		if ( array[0].length < expLen ) {
+			obj._dataMem = expLen + obj._separator + array[0];
+			return false;
+		} else {
+
+			if ( expLen > 0) {
+				const cData = array[0].substring(0, expLen);
+
+				// if ( obj._validateIat( cData ) ) { // Optional
+					obj._onParsed( cData );
+				// }
+			}
+
+			if ( array.length > 1 ) {
+				array.shift();
+				obj._parseChunks(array);
+			} else {
+				if ( array[0].substring(expLen, array[0].length).replace(/\s/g, '').length != 0 ) {
+					obj._dataMem = array[0];
+				}
+				return false;
+			}
+			
+		}
+
+	}
+
+
+	_validateIat(rawString) {
+		const obj = this;
+
+		let jData;
+
+		try {
+			jData = JSON.parse(rawString);
+		} catch(er) {
+			console.warn(`Data received is not valid JSON.`, `Timestamp validation was not done... Returning data anyway.`);
+			console.error(er);
+			return true;
+		}
+
+		if ( !('iat' in jData) ) {
+			console.warn(`Data received does not match expected format: {"iat": Unixtime, "payload": Object}`, `Timestamp validation was not done... Returning data anyway.`);
+			return true;
+		}
+
+		if ( !((new Date(jData.iat)).getTime() > 0) ) {
+			console.warn(`Timestamp received is invalid.`, `Timestamp validation was not done... Returning data anyway.`);
+			return true;
+		}
+
+		if (jData.iat < obj._lastIat) {
+			console.warn(`Data received is older than last read.`, `Data will be not returned.`);
+			return false;
+		} else {
+			obj._lastIat = jData.iat;
+			return true;
+		}
+
+	}
+
+
+
+	_ajaxReq(protocol) {
+
+		let pr;
+
+		switch (protocol) {
+			case 'http:':
+				pr = http;
+				break;
+			case 'https:':
+				pr = https;
+				break;
+			default:
+				pr = false;
+				break;
+		}
+
+		return pr;
+	}
+
+
+
+	_gBindKey() {
+
+		const obj = this;
+
+		return new Promise( (resolve, reject) => {
+			
+			if (obj._bindURL == '') {
+				resolve();
+				return;
+			}
+
+
+			// console.info('Binding...')
+
+			const parseUrl = new URL(obj._bindURL);
+
+			const options = {
+				hostname: parseUrl.hostname,
+				port: parseUrl.port,
+				path: parseUrl.pathname,
+				headers: {
+					// 'Cookie': 'PHPSESSID=71407ba645ab1ffef88ccaa4232cfb62'
+				}
+			}
+
+			let bKey = '';
+
+			const getKey = obj._ajaxReq(parseUrl.protocol).get(options, (res) => {
+
+				res.on('data', (d) => {
+					bKey += d.toString();
+				});
+
+			});
+
+
+
+			getKey.on('error', (e) => {
+				console.error("_gBindKey on.error:", e);
+			});
+
+
+
+			getKey.on('close', (e) => {
+				console.log("_gBindKey on.close:", e);
+
+				// SIMILAR TO readyState 4 (could be successful or aborted...)
+
+				if ( !getKey.res ) {
+					console.error(`Unbinding...`);
+					resolve();
+					return;
+				}
+
+				// console.log(getKey.res.statusCode, bKey)
+
+				if (getKey.res.statusCode >= 200 && getKey.res.statusCode < 300) {
+
+					// console.info('Binded!')
+
+					// Inject user binding to body
+					const tmpObj = JSON.parse(obj._XHR.data) || {};
+					tmpObj.bind_key = bKey;
+					obj._XHR.data = JSON.stringify(tmpObj);
+
+					resolve();
+					return;
+
+				} else {
+					console.error(`Unbinding...`);
+					resolve();
+					return;
+				}
+
+			});
+
+			getKey.end();
+
+		} );
+
+	}
+
+
+
+	_subscribe() {
+
+		const used = process.memoryUsage();
+		for (let key in used) {
+			console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+		}
+
+		if (this._stopped) return false;
+		const obj = this;
+
+		if (obj._httpCall) {
+			obj._httpCall = null;
+			obj._subscribe();
+			return false;
+		}
+
+
+		obj._gBindKey()
+			.then( () => {
+
+				const parseUrl = new URL(obj._XHR.url);
+
+				obj._XHR.headers['Content-Length'] = Buffer.byteLength(obj._XHR.data);
+
+				const options = {
+					hostname: parseUrl.hostname,
+					port: parseUrl.port,
+					path: parseUrl.pathname,
+					method: obj._XHR.method,
+					headers: obj._XHR.headers
+				}
+
+
+				// SIMILAR TO readyState 0 ?
+				obj._onStChange({value:0,state:"DISCONNECTED"});
+				obj._subscribed = false;
+
+				obj._httpCall = obj._ajaxReq(parseUrl.protocol).request(options, (res) => {
+
+					// console.log('statusCode:', res.statusCode);
+					// console.log('headers:', res.headers);
+
+					obj._httpCall.responseText = '';
+					obj._responseLen = 0;
+
+					res.on('data', (d) => {
+
+						// SIMILAR TO readyState 3
+
+						if (res.statusCode >= 200 && res.statusCode < 300) {
+							obj._onStChange({value:2,state:"CONNECTED"});
+
+							if ( !obj._subscribed ) {
+								obj._subscribed = true;
+								obj._onSubscribe();
+							}
+
+							obj._resTimeoutRestart();
+						} else {
+							obj._onStChange({value:0,state:"DISCONNECTED"});
+						}
+
+						obj._httpCall.responseText += d.toString();
+						obj._logicOnChunk();
+					});
+
+					res.on('end', () => {
+						// DATA FINISHED TRANSFERING
+
+						// SIMILAR TO readyState 4 (successful)
+
+						if (res.statusCode >= 200 && res.statusCode < 300) {
+
+							if (obj._httpCall.responseText.length == 0) {
+								obj._onStChange({value:0,state:"DISCONNECTED"});
+								obj._subscribed = false;
+								console.error('No bytes were received. Breaking loop... Retrying in 5 seconds.');
+								obj._retryTimeout = setTimeout( () => { obj._subscribe() }, 5000);
+								return false;
+							}
+
+							obj._subscribe(); // RECONNECTING!
+
+						} else {
+							obj._onStChange({value:0,state:"DISCONNECTED"});
+							obj._subscribed = false;
+							if ( !this._stopped ) {
+								console.error(`HTTP Status: ${res.statusCode}.`, 'Breaking loop... Retrying in 5 seconds.');
+								obj._retryTimeout = setTimeout( () => { obj._subscribe() }, 5000);
+							}
+							return false;
+						}
+					});
+				});
+
+
+
+				obj._httpCall.on('error', (e) => {
+					console.error("_httpCall on.error:", e);
+
+					obj._onStChange({value:0,state:"DISCONNECTED"});
+					obj._subscribed = false;
+				});
+
+
+
+				obj._httpCall.on('connect', (e) => {
+					console.log("_httpCall on.connect:", e);
+				});
+
+				obj._httpCall.on('continue', (e) => {
+					console.log("_httpCall on.continue:", e);
+				});
+
+				obj._httpCall.on('information', (e) => {
+					console.log("_httpCall on.information:", e);
+				});
+
+				obj._httpCall.on('response', (e) => {
+					console.log("_httpCall on.response:", /*e*/);
+
+					// SIMILAR TO readyState 2 (with response! so headers received)
+				});
+
+				obj._httpCall.on('socket', (e) => {
+					console.log("_httpCall on.socket:", /*e*/);
+
+					// SIMILAR TO readyState 1
+					obj._onStChange({value:1,state:"WAITING"});
+				});
+
+				obj._httpCall.on('timeout', (e) => {
+					console.log("_httpCall on.timeout:", e);
+				});
+
+				obj._httpCall.on('upgrade', (e) => {
+					console.log("_httpCall on.upgrade:", e);
+				});
+
+				obj._httpCall.on('close', (e) => {
+					console.log("_httpCall on.close:", e);
+
+					// SIMILAR TO readyState 4 (could be successful or aborted...)
+
+					obj._onStChange({value:0,state:"DISCONNECTED"});
+					obj._subscribed = false;
+
+					obj._resTimeoutCancel();
+
+					if ( !obj._httpCall ) {
+						console.error('Connection failed!');
+						return;
+					}
+					
+					if (obj._httpCall.destroyed) {
+						console.error('Connection aborted!');
+						if ( !this._stopped ) {
+							console.error('Retrying in 5 seconds.');
+							obj._retryTimeout = setTimeout( () => { obj._subscribe() }, 5000);
+						}
+					}
+
+				});
+
+				obj._httpCall.on('finish', (e) => {
+					console.log("_httpCall on.finish:", e);
+
+					// SIMILAR TO readyState 2 (no response yet... so no headers yet...)
+				});
+
+
+
+				// post the data
+				obj._httpCall.write(obj._XHR.data);
+
+				obj._httpCall.end();
+
+			})
+
+	}
+
+
+
+	_resTimeoutRestart() {
+		this._resTimeoutCancel();
+		this._resTimeoutStart();
+	}
+
+
+
+	_resTimeoutStart() {
+		const obj = this;
+
+		const responseTimeout = 1000 * 60; // 1 minute
+
+		obj._resTimeout = setTimeout( () => { obj.abort(); }, responseTimeout);
+	}
+
+
+
+	_resTimeoutCancel() {
+		clearTimeout(this._resTimeout);
+		delete this._resTimeout;
+	}
+
+
+
+	/*_decomposeURL(fullURL) {
+		const obj = this;
+
+		var lnk = document.createElement("a");
+		lnk.href = fullURL;
+		// lnk.href = 'https://who:passs@example.com:810/path/index.html?message=hello&who=world#xD';
+
+		obj._url = lnk.href.replace(lnk.search, '').replace(lnk.hash, '');
+
+		obj._urlQuery = lnk.search.split('&');
+
+		obj._urlQuery.forEach( (el, index, node) => {
+			if (el.length == 0) { node.splice(index, 1) }
+		} );
+		
+		if (obj._urlQuery.length > 0 && obj._urlQuery[0].charAt(0) == '?') {
+			obj._urlQuery[0] = obj._urlQuery[0].substring(1);
+		}
+
+		obj._urlHash = lnk.hash;
+	}*/
+
+
+
+	/*_composeURL() {
+		const obj = this;
+
+		if ( obj._urlTStampKey != '' ) {
+			obj._urlQuery.push(`${obj._urlTStampKey}=${obj._lastParsedTime}`);
+			obj._urlTStampKey = '';
+		}
+
+		const base = obj._url;
+		const query = obj._urlQuery.length > 0 ? '?' + obj._urlQuery.join('&') : '';
+		const hash = obj._urlHash;
+
+		const url = base + query + hash;
+
+		return url;
+	}*/
+
+
+
+
+
+	/////////////////////////////////////////////////
+	//////////////////// SETTERS ////////////////////
+
+	setURL(stringOrObj) {
+		this._XHR = this._setHTTPRequest(stringOrObj);
+	}
+
+	setBindingKeyUrl(urlString) { // Bind connection to user_id (authorization layer).
+
+		if ( typeof urlString !== 'string') {
+			console.error('Bind url should be a string.', 'Binding was not set.');
+			return;
+		}
+
+		this._bindURL = urlString;
+	}
+
+	unsetBindingKey() { // Unbind connection from user_id.
+		this._bindURL = '';
+	}
+
+
+
+
+
+}
+
+module.exports = NoPollSubscriber;
