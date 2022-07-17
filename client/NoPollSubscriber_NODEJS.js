d181e484238fae661c7b78e0304b6b04bba601c3
Roy Mdr <go_rojo@hotmail.com>
2022-07-17 17:37:58 -0500

    Commented out memory usage

--- client/NoPollSubscriber_NODEJS.js
+++ client/NoPollSubscriber_NODEJS.js
@@ -414,10 +414,12 @@ class NoPollSubscriber {
 
 	_subscribe() {
 
-		const used = process.memoryUsage();
-		for (let key in used) {
-			console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
-		}
+		/* DEBUG MEMORY USAGE */
+		// const used = process.memoryUsage();
+		// for (let key in used) {
+		// 	console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+		// }
+		/* ! DEBUG MEMORY USAGE */
 
 		if (this._stopped) return false;
 		const obj = this;
